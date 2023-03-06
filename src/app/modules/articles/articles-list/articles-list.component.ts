86ff65acc61e554f1c3feb6b4028645f04f4b2ac
Sergio romero <sergior500@gmail.com>
2023-03-06 10:33:27 +0100

    subida alberto

--- src/app/modules/articles/articles-list/articles-list.component.ts
+++ src/app/modules/articles/articles-list/articles-list.component.ts
@@ -1,11 +1,9 @@
-import { ChangeDetectorRef, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { ArticleService } from '../article-Services/article.service';
 import { Article, Content } from '../../../interfaces/article.interface';
 import { Observable } from 'rxjs';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table'
-import { MatSort } from '@angular/material/sort';
-import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-articles-list',
@@ -14,137 +12,47 @@ import Swal from 'sweetalert2';
 })
 export class ArticlesListComponent implements OnInit {
 
-  articles: Content[] = [];
+  articles:Content[] = [];
   error:boolean = true;
 
-  cossas:boolean = true;
-
-
-  displayedColumns = ['id', 'name', 'price', 'stock', 'description','category','button'];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  obs!: Observable<any>;
   dataSource!: MatTableDataSource<Content>;
 
-  @Input()
-  filterValue : string = "";
-  
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
-
-  // @ViewChild(MatPaginator) paginator!: MatPaginator;
-  // obs!: Observable<any>;
-  // dataSource!: MatTableDataSource<Article>;
-
-  constructor(private articleService: ArticleService , /*, private changeDetectorRef: ChangeDetectorRef*/) {
-
-   }
+  constructor(private articleService: ArticleService, private changeDetectorRef: ChangeDetectorRef) { }
 
 
 
   ngOnInit(): void {
     this.articleList();
     
-    if(!this.cossas){
-      this.displayedColumns = ['id', 'name', 'price', 'stock', 'description','category']
+
+    console.log(this.obs)
+    console.log(this.articles)
+    
+  }
+
+  ngOnDestroy() {
+    if (this.dataSource) { 
+      this.dataSource.disconnect(); 
     }
-    // console.log(this.obs)
-    // console.log(this.articles)
-    
   }
-
-
-  applyFilter() {
-    this.filterValue = this.filterValue.trimStart();
-    this.filterValue = this.filterValue.toLowerCase(); // Datasource defaults to lowercase matches
-    this.dataSource.filter = this.filterValue;
-  }
-
-//   pageChangeEvent(event: number){
-
-//     this.p = event;
-    
-//     console.log(event)
-
-//     this.articleList();
-
-// }
-  // ngOnDestroy() {
-  //   if (this.dataSource) { 
-  //     this.dataSource.disconnect(); 
-  //   }
-  // }
   
   articleList(){
-    // this.articleService.articleList()
-    //   .subscribe({
-    //     next: (resp) => {
-    //       this.dataSource = new MatTableDataSource<Article>(resp)
-    //       this.changeDetectorRef.detectChanges();
-
-    //       this.dataSource.paginator = this.paginator;
-    //       this.obs = this.dataSource.connect();
-    //       this.error = false;
-    //       console.log(resp)
-          
-    //     }
-    //   })
-    //   console.log(this.articles)
-
     this.articleService.articleList()
       .subscribe({
-        next:(resp)=>{
-          this.dataSource = new MatTableDataSource(resp.content);
+        next: (resp) => {
+          this.dataSource = new MatTableDataSource<Content>(resp.content)
+          this.changeDetectorRef.detectChanges();
+
           this.dataSource.paginator = this.paginator;
-          this.dataSource.sort = this.sort;
-          console.log(this.dataSource)
-          // this.articles = resp.content;
-          // this.total = resp.totalPages;
-          // this.error = false;
-          // console.log(resp.content)
-          // console.log(this.articles)
+          this.obs = this.dataSource.connect();
+          this.error = false;
+          console.log(resp)
+          
         }
-    })
+      })
+      console.log(this.articles)
   }
 
-    delete(id:number){
-      const swalWithBootstrapButtons = Swal.mixin({
-        customClass: {
-          confirmButton: 'btn btn-success',
-          cancelButton: 'btn btn-danger'
-        },
-        buttonsStyling: false
-      })
-      
-      swalWithBootstrapButtons.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonText: 'Yes, delete it!',
-        cancelButtonText: 'No, cancel!',
-        reverseButtons: true
-      }).then((result) => {
-        if (result.isConfirmed) {
-          this.articleService.deleteArticle(id)
-            .subscribe({
-              next:(res) => {
-              }
-            })
-            swalWithBootstrapButtons.fire(
-              'Deleted!',
-              'Your file has been deleted.',
-              'success'
-            ).then(() => {
-              window.location.reload()
-            })
-        } else if (
-          /* Read more about handling dismissals below */
-          result.dismiss === Swal.DismissReason.cancel
-        ) {
-          swalWithBootstrapButtons.fire(
-            'Cancelled',
-            'Your file is safe :)',
-            'error'
-          )
-        }
-      })
-    }
 }
